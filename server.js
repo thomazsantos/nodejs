6403c7c97fdd46492c0853b8b00038535325020a
Thomaz Luiz Santos <thomaz.santos@gmail.com>
2020-11-05 09:11:40 +0100

    remove trash files

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-
-// listening port
-const PORT=process.env.PORT||8080;
-app.listen(PORT,()=>{
-    console.log(`app is live at ${PORT}`);
-});
-
-module.exports = app;
